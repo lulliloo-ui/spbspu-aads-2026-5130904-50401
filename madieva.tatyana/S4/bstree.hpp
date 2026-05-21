f26fa531b1e06699368a717203259a472b0e8ddd
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2026-05-21 10:25:58 +0300

    BSTree, push

--- madieva.tatyana/S4/bstree.hpp
+++ madieva.tatyana/S4/bstree.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <utility>
+#include <cstddef>
 
 namespace madieva {
   template< class Key, class Value, class Compare >
@@ -10,7 +11,9 @@ namespace madieva {
 
   template < class Key, class Value, class Compare >
   class Node {
-    Node< Key, Value, Compare > * left, * right, * parent;
+    Node< Key, Value, Compare > * left;
+    Node< Key, Value, Compare > * right;
+    Node< Key, Value, Compare > * parent;
     std::pair< Key, Value > data;
     friend class BSTree< Key, Value, Compare >;
   public:
@@ -23,16 +26,13 @@ namespace madieva {
   class BSTree {
     Node< Key, Value, Compare> * root_;
     Compare cmp_;
-
   public:
+    BSTree();
     void push(Key k, Value v);
     const Value & get(const Key k) const;
     Value & get(const Key k);
-    drop(Key k);
+    void drop(Key k);
   };
-
-
-
 
   template < class Key, class Value, class Compare>
   Node< Key, Value, Compare >::Node(const Key & k,const Value & v) :
@@ -49,6 +49,50 @@ namespace madieva {
     right(nullptr),
     parent(nullptr)
   {}
+
+  template< typename Key, typename Value, typename Compare >
+  BSTree< Key, Value, Compare >::BSTree() :
+    root_(nullptr),
+    cmp_()
+  {}
+
+  template < class Key, class Value, class Compare>
+  void BSTree< Key, Value, Compare >::push(Key k, Value v)
+  {
+    Node< Key, Value, Compare > * n = new Node< Key, Value, Compare >(k, v);
+    if (!root_) {
+      root_ = n;
+      return;
+    }
+    Node< Key, Value, Compare > * curr = root_;
+    while (curr) {
+      if (cmp_(n->data.first, curr->data.first)) {
+        if (!curr->left) {
+          curr->left = n;
+          n->parent = curr;
+          return;
+        }
+        curr = curr->left;
+      } else if (cmp_(curr->data.first, n->data.first)) {
+        if (!curr->right) {
+          curr->right = n;
+          n->parent = curr;
+          return;
+        }
+        curr = curr->right;
+      } else {
+        delete n;
+        return;
+      }
+    }
+  }
+
+  template < class Key, class Value, class Compare>
+  const Value & BSTree< Key, Value, Compare >::get(const Key k) const
+  {
+
+  }
+
 
 }
 
